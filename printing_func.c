9adf9b578e8ec0285f14b3a205d990425d137533
AlaaCodes6 <alaacodes6@gmail.com>
2023-11-15 12:38:51 -0800

    printing functions added

---
+++ printing_func.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+		_putchar(str[c]);
+		_putchar('\n');
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character
+ * Return: 1 on Success.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
